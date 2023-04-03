cb61b81b92fa90317196b7dd35b45f49d17f4013
kodxxl <111854579+kodxxl@users.noreply.github.com>
2023-04-03 16:33:50 +0400

    Update firewall-forward-to-kapi.sh

--- firewall-forward-to-kapi.sh
+++ firewall-forward-to-kapi.sh
@@ -5,6 +5,8 @@ sysctl -p
 
 EXTERNAL_IP="84.201.154.102"
 INTERNAL_IP="192.168.10.35"
-PORT=6443
+EXTERNAL_PORT=6443
+INTERNAL_PORT=6443
 
 iptables -t nat -A POSTROUTING -p tcp -d ${EXTERNAL_IP} --dport ${PORT} -j SNAT --to-source ${INTERNAL_IP}
+iptables -t nat -A PREROUTING -p tcp --dport ${EXTERNAL_PORT} -j DNAT --to-destination ${INTERNAL_IP}:${INTERNAL_PORT}
