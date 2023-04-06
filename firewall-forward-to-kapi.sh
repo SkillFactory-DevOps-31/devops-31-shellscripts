608ed55376cb8ce28daf67c75d40bca3d69d2c97
kodxxl <111854579+kodxxl@users.noreply.github.com>
2023-04-06 08:37:39 +0400

    Update firewall-forward-to-kapi.sh

--- firewall-forward-to-kapi.sh
+++ firewall-forward-to-kapi.sh
@@ -3,8 +3,12 @@
 echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 sysctl -p
 
-INTERNAL_IP="192.168.10.35"
-EXTERNAL_PORT=6443
-INTERNAL_PORT=6443
+INTERNAL_CLUSTER_IP="192.168.10.35"
+EXTERNAL_API_PORT=6443
+INTERNAL_API_PORT=6443
 
-iptables -t nat -A PREROUTING -p tcp --dport ${EXTERNAL_PORT} -j DNAT --to-destination ${INTERNAL_IP}:${INTERNAL_PORT}
+EXTERNAL_APP_PORT=8080
+INTERNAL_APP_PORT=13531
+
+iptables -t nat -A PREROUTING -p tcp --dport ${EXTERNAL_API_PORT} -j DNAT --to-destination ${INTERNAL_CLUSTER_IP}:${INTERNAL_API_PORT}
+iptables -t nat -A PREROUTING -p tcp --dport ${EXTERNAL_APP_PORT} -j DNAT --to-destination ${INTERNAL_CLUSTER_IP}:${INTERNAL_APP_PORT}
